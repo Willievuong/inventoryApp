4ec913021d41adcb1084f153abcac7327b53695d
William Vuong <willievuong@gmail.com>
2020-03-17 22:32:23 -0700

    First HTTP Request via Volley in Android

--- app/src/main/java/com/example/inventorytracker/MainActivity.kt
+++ app/src/main/java/com/example/inventorytracker/MainActivity.kt
@@ -5,7 +5,12 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log.d
+import android.widget.TextView
 import androidx.lifecycle.lifecycleScope
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -36,5 +41,25 @@ class MainActivity : AppCompatActivity() {
 //            d("will", "the message is: $message")
 //            lastSavedProduct.text = message
 //        }
+
+//        val textView = findViewById<TextView>(R.id.text)
+
+    // Instantiate the RequestQueue.
+        val queue = Volley.newRequestQueue(this)
+        val url = "http://www.google.com"
+
+    // Request a string response from the provided URL.
+        val stringRequest = StringRequest(
+            Request.Method.GET, url,
+            Response.Listener<String> { response ->
+                // Display the first 500 characters of the response string.
+                d("will", "the message is:${response}")
+//                textView.text = "Response is: ${response.substring(0, 500)}"
+            },
+            Response.ErrorListener { d("will", "That didn't work!") })
+
+    // Add the request to the RequestQueue.
+        queue.add(stringRequest)
+
     }
 }
